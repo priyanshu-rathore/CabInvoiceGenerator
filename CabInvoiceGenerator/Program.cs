c9f97099b16d24fc0a2694df675e46ecf6e72090
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-02-06 22:16:47 +0530

    Calculate total fare for the journey

---
+++ CabInvoiceGenerator/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace CabInvoiceGenerator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Welcome to Cab Invoice Generator!");
+            InvoiceGenerator uc1 = new();
+            double fare1 = uc1.CalculateFare(10, 30);
+            Console.WriteLine("Total Fare for the Journey is :" + fare1);
+
+        }
+    }
+}
