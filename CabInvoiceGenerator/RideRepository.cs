862ba26b407ef91b5bcc53949bddce906e90cd08
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-02-06 22:27:22 +0530

    Generating invoice using User ID

---
+++ CabInvoiceGenerator/RideRepository.cs
@@ -0,0 +1,38 @@
+﻿namespace CabInvoiceGenerator
+{
+    public class RideRepository
+    {
+        Dictionary<string, MultipleRides[]> DictionaryOfRideDetails = new Dictionary<string, MultipleRides[]>();
+        public RideRepository()
+        {
+            this.DictionaryOfRideDetails = new Dictionary<string, MultipleRides[]>();
+        }
+        public void AddRides(string userId, MultipleRides[] rides)
+        {
+            if (!DictionaryOfRideDetails.ContainsKey(userId))
+            {
+                DictionaryOfRideDetails.Add(userId, rides);
+            }
+            else
+            {
+                Console.WriteLine("Rides are Null");
+            }
+        }
+        public MultipleRides[] GetRide(string userId)
+        {
+            foreach (var data in DictionaryOfRideDetails)
+            {
+                if (data.Key == userId)
+                {
+                    Console.WriteLine("User ID is :" + userId);
+                    return data.Value;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid User ID");
+                }
+            }
+            return null;
+        }
+    }
+}
