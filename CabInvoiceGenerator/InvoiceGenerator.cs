b9498d9cf80f1e83ebe3c9c5277dd875c3f18690
priyanshu-rathore <priyanshurathore132@gmail.com>
2023-02-06 22:30:14 +0530

    Fare for normal and premium rides

--- CabInvoiceGenerator/InvoiceGenerator.cs
+++ CabInvoiceGenerator/InvoiceGenerator.cs
@@ -15,12 +15,25 @@ namespace CabInvoiceGenerator
 
         public double CalculateFare(double Distance, int Time)
         {
+
             count++;
             double Fare = (CostPerKm * Distance) + (CostPerMinute * Time);
             return Math.Max(Fare, MinimumFare);
         }
-        public double CalculateFare(MultipleRides[] ride)
+        public double CalculateFare(MultipleRides[] ride, string RideType)
         {
+            if (RideType == "Normal")
+            {
+                CostPerKm = 10;
+                CostPerMinute = 1;
+                MinimumFare = 5;
+            }
+            else if (RideType == "Premium")
+            {
+                CostPerKm = 15;
+                CostPerMinute = 2;
+                MinimumFare = 20;
+            }
             double totalfare = 0;
             foreach (MultipleRides rides in ride)
             {
@@ -29,11 +42,12 @@ namespace CabInvoiceGenerator
             }
             return totalfare;
         }
-        public void InvoiceSummary(MultipleRides[] ride)
+        public void InvoiceSummary(MultipleRides[] ride, string RideType)
         {
+
             foreach (MultipleRides rides in ride)
             {
-
+                Console.WriteLine("Ride Type :" + RideType);
                 Console.WriteLine("Distance Travelled :" + rides.Distance);
                 Console.WriteLine("Time Taken :" + rides.Time);
                 Console.WriteLine("Fare for ride is :" + CalculateFare(rides.Distance, rides.Time));
